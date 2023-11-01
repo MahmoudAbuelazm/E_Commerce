dfe80d34c5e8c6d333e82086abfd2c9d2042ddc5
Mahmoud khairy <74417372+mahmoudkhairy402@users.noreply.github.com>
2023-11-01 23:10:05 +0200

    Update Notfound.jsx
    404 page

--- Front/client/src/Pages/Notfound/Notfound.jsx
+++ Front/client/src/Pages/Notfound/Notfound.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 /**
  * @author
@@ -7,8 +8,29 @@ import React from "react";
 
 const Notfound = (props) => {
   return (
-    <div>
-      <div className="alert alert-danger">ERROR 404..Notfound</div>
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{
+        flexDirection: "column",
+        height: "80vh",
+        width: "100%",
+        gap: "30px",
+      }}
+    >
+      <h1
+        className=" text-center"
+        style={{ fontSize: "110px", fontWeight: "500" }}
+      >
+        404 Not found
+      </h1>
+
+      <Link
+        to={"/home"}
+        className="btn btn-danger border-0 py-3 px-5  "
+        style={{ backgroundColor: "#912b22", textTransform: "capitalize" }}
+      >
+        back to home
+      </Link>
     </div>
   );
 };
