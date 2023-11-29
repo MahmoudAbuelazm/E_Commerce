7f3a4c905b6d5a59e9bf5ff93aeeb6c0efa9dcd8
john <74468815+john-ihab@users.noreply.github.com>
2023-11-29 20:38:59 +0200

    cart 0

---
+++ Front/client/src/components/store.js
@@ -0,0 +1,11 @@
+// store.js
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './cartSlice';
+
+const store = configureStore({
+    reducer: {
+        cart: cartReducer,
+    },
+});
+
+export default store;
