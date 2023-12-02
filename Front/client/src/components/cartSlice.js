b6311c29899fbd7f048257d14c6f9fa7869b038a
‪Ehab Yousef‬‏ <74505597+ehabyousef@users.noreply.github.com>
2023-12-02 23:57:27 +0200

    Merge branch 'front-branch' of https://github.com/MahmoudAbuelazm/E_Commerce into front-branch

--- Front/client/src/components/cartSlice.js
+++ Front/client/src/components/cartSlice.js
@@ -7,24 +7,18 @@ export const cartSlice = createSlice({
     reducers: {
         addToCart: (state, action) => {
             const findProduct = state.find((product) => product.id === action.payload.id);
-            if( findProduct ) {
+            if (findProduct) {
                 findProduct.quantity += 1;
             } else {
-                const productClone = {...action.payload, quantity: 1}
+                const productClone = { ...action.payload, quantity: 1 }
                 state.push(productClone);
             }
         },
         deleteFromCart: (state, action) => {
             return state.filter((product) => product.id !== action.payload.id);
         }
-        // ,
-        // removeWordAdd: (state, action) => {
-        //     return state.filter((product) => product.id !== action.payload.id);
-            
-            
-        // }
     },
 });
 
-export const { addToCart, deleteFromCart} = cartSlice.actions;
+export const { addToCart, deleteFromCart } = cartSlice.actions;
 export default cartSlice.reducer;
