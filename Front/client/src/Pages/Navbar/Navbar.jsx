c650db79ff6f114d9f8b21f49599ed13eead9d8c
Mahmoud khairy <74417372+mahmoudkhairy402@users.noreply.github.com>
2023-12-10 18:01:23 +0200

    update navbar autorization

--- Front/client/src/Pages/Navbar/Navbar.jsx
+++ Front/client/src/Pages/Navbar/Navbar.jsx
@@ -73,50 +73,7 @@ function Navbar(props) {
                 </Link>
               </li>
               <li className="nav-item">
-                {props.isLoggedIn ? (
-                  <Link
-                    style={{
-                      borderRadius: "6px",
-                      backgroundColor: "#912b22",
-                      color: "#fff",
-                      paddingLeft: "7px",
-                      paddingRight: "10px",
-                      maxWidth: "50px",
-                      // position: "absolute",
-                      // right: "1%",
-                    }}
-                    title="logout"
-                    onClick={() => {
-                      handleLinkClick("logout");
-                      props.setIsLoggedIn(false);
-                      localStorage.setItem("isLoggedIn", "false");
-                      localStorage.setItem("userToken", "");
-                    }}
-                    className={`nav-link ms-sm-0 ms-lg-2 mt-sm-2 mt-lg-0 ${
-                      activeLink === "logout" ? `${Style.active}` : ""
-                    }`}
-                    to={"/login"}
-                  >
-                    <svg
-                      style={{ marginLeft: "5px" }}
-                      xmlns="http://www.w3.org/2000/svg"
-                      width={20}
-                      height={20}
-                      fill="currentColor"
-                      className="bi bi-box-arrow-left"
-                      viewBox="0 0 16 16"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"
-                      />
-                      <path
-                        fillRule="evenodd"
-                        d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"
-                      />
-                    </svg>
-                  </Link>
-                ) : (
+                {!props.isLoggedIn && (
                   <Link
                     onClick={() => {
                       handleLinkClick("login");
@@ -131,7 +88,7 @@ function Navbar(props) {
                 )}
               </li>
             </ul>
-            <span className="d-flex align-items-center gap-3">
+            <span className="d-flex align-items-center gap-3 justify-content-evenly">
               <div className={Style.InputContainer}>
                 <input
                   type="text"
@@ -153,63 +110,114 @@ function Navbar(props) {
                   </svg>
                 </label>
               </div>
-              <Link
-                onClick={() => {
-                  handleLinkClick("favourite");
-                }}
-                className={`nav-link ${
-                  activeLink === "favourite" ? `${Style.active}` : ""
-                }`}
-                to="/favourite"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  fill="#000"
-                  className={`bi bi-heart`}
-                  viewBox="0 0 16 16"
-                >
-                  <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-                </svg>
-              </Link>
 
-              <Link
-                onClick={() => {
-                  handleLinkClick("cart");
-                }}
-                className={`nav-link ${
-                  activeLink === "cart" ? `${Style.active}` : ""
-                }`}
-                to="/cart"
-              >
-                {cart.length > 0 ? (
-                  <LiaCartArrowDownSolid className={` ${Style.cartIcon} `} />
-                ) : (
+              {/* fav */}
+              {props.isLoggedIn && (
+                <Link
+                  onClick={() => {
+                    handleLinkClick("favourite");
+                  }}
+                  className={`nav-link ${
+                    activeLink === "favourite" ? `${Style.active}` : ""
+                  }`}
+                  to="/favourite"
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="20"
                     height="20"
                     fill="#000"
-                    style={{ marginBottom: "4px" }}
-                    className={`bi bi-cart  `}
+                    className={`bi bi-heart`}
                     viewBox="0 0 16 16"
                   >
-                    <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
+                    <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
                   </svg>
-                )}
-              </Link>
+                </Link>
+              )}
+              {/* cart */}
+              {props.isLoggedIn && (
+                <Link
+                  onClick={() => {
+                    handleLinkClick("cart");
+                  }}
+                  className={`nav-link ${
+                    activeLink === "cart" ? `${Style.active}` : ""
+                  }`}
+                  to="/cart"
+                >
+                  {cart.length > 0 ? (
+                    <LiaCartArrowDownSolid className={` ${Style.cartIcon} `} />
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="#000"
+                      style={{ marginBottom: "4px" }}
+                      className={`bi bi-cart  `}
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
+                    </svg>
+                  )}
+                </Link>
+              )}
 
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className={`bi bi-person-fill ${Style.avatar}`}
-                viewBox="0 0 16 16"
-              >
-                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
-              </svg>
+              {props.isLoggedIn && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className={`bi bi-person-fill ${Style.avatar}`}
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+                </svg>
+              )}
+
+              {props.isLoggedIn && (
+                <Link
+                  style={{
+                    borderRadius: "6px",
+                    backgroundColor: "#912b22",
+                    color: "#fff",
+                    padding: "7px",
+                    maxWidth: "100px",
+                  }}
+                  title="logout"
+                  onClick={() => {
+                    handleLinkClick("logout");
+                    props.setIsLoggedIn(false);
+                    localStorage.setItem("isLoggedIn", "false");
+                    localStorage.setItem("userToken", "");
+                  }}
+                  className={`nav-link ms-sm-0 ms-lg-2 mt-sm-2 mt-lg-0 ${
+                    activeLink === "logout" ? `${Style.active}` : ""
+                  }`}
+                  to={"/login"}
+                >
+                  Logout
+                  <svg
+                    style={{ marginLeft: "5px" }}
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={20}
+                    height={20}
+                    fill="currentColor"
+                    className="bi bi-box-arrow-left"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"
+                    />
+                    <path
+                      fillRule="evenodd"
+                      d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"
+                    />
+                  </svg>
+                </Link>
+              )}
             </span>
           </div>
         </div>
